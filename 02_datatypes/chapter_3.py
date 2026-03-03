07d06aff6b2dd5af6c82495922da166ae01c3edc
iamvishaldev <vishalyadavdev1@example.com>
2026-03-03 22:31:20 +0530

    done with data type

---
+++ 02_datatypes/chapter_3.py
@@ -0,0 +1,37 @@
+# Interger
+
+black_tea_grams = 14
+ginger_grams = 3
+
+total_grams = black_tea_grams + ginger_grams
+
+print(f"Total grams of tea is {total_grams}")
+
+remaing_tea = black_tea_grams * ginger_grams
+
+print(f"Total grams of remaining tea is in * {remaing_tea}")
+
+
+remaing_tea = black_tea_grams - ginger_grams
+
+
+print(f"Total grams of remaining tea is {remaing_tea}")
+
+total_tea_bags = 7
+pots = 4
+bags_per_pots = total_tea_bags // pots
+
+print(f'while tea bags per pods',bags_per_pots)
+
+total_cadamon_pods = 10
+
+pods_per_cup = 3
+
+leftover_pods = total_cadamon_pods % pods_per_cup
+
+print(f'Leftover C pods {leftover_pods}')
+
+
+total_tea_harvested = 100_1000_100
+
+print(f"total_tea_harvested {total_tea_harvested}")
