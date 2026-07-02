638e2a6b9c73bfe2131ef963d52b4be8e0b02394
Vishal <vishalplexitech@gmail.com>
2026-07-02 14:28:45 +0530

    learning: add basics revision (data types, REPL calculator)

---
+++ Revision/num.py
@@ -0,0 +1,44 @@
+# Integers
+
+black_tea_grams = 14
+ginger_grams = 3
+
+total_grams = black_tea_grams + ginger_grams
+print(f"total",total_grams)
+
+remaing_tea = black_tea_grams - ginger_grams
+print(f"remaining",remaing_tea)
+
+milk_litre = 7
+servings = 4
+milk_per_serving = milk_litre / servings
+print(f"per serving",milk_per_serving)
+
+total_tea_bags = 7
+
+pots = 4
+
+bags_per_pods = total_tea_bags // pots
+print(f"bags per pods",bags_per_pods)
+
+total_cardamom_pods = 10
+pods_per_cup = 3
+
+cups = total_cardamom_pods % pods_per_cup
+
+print(f"cups",cups)
+
+base_flavour_strength = 2
+scale_flavour = 3
+powerful_flavour = base_flavour_strength ** scale_flavour
+print(f"scaled flavour strength {powerful_flavour}")
+
+# Boolean
+
+is_boiling = True
+
+stri_count = 5
+
+# Real floating -> decimal
+
+# complex number 2+3j
---
+++ Revision/revision.py
@@ -0,0 +1,16 @@
+print(f"Vishal")
+a = 5
+b = 10
+print(f"add",a+b)
+
+# lENGTH OF A STRING
+
+str = "vishal"
+print(len(str))
+
+# Mini Project: REPL Calculator
+
+num1 = int(input("Enter your first num:"))
+num2 = int(input("Enter your second num:"))
+
+print(f"Total: {num1 + num2}")
