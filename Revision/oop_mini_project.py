33c870a66b2c38dea6bde75ed09324ffb3eeb8f8
Vishal <vishalplexitech@gmail.com>
2026-07-29 17:37:51 +0530

    mini

--- Revision/oop_mini_project.py
+++ Revision/oop_mini_project.py
@@ -1,12 +1,31 @@
 class Book:
-
-  def __init__(self,title,author):
+  def __init__(self,book_id,title,price):
+    self.book_id = book_id
     self.title = title
-    self.author = author
+    self.price = price
+    self.is_borrowed = False
 
-  def display(self):
-    print(f"The Book Title is {self.title} by author {self.author}")
+  def display_book(self):
+    print(f"Book ID {self.book_id}")
+    print(f"Title: {self.title}")
+    print(f"Price: ₹{self.price}")
+    print(f"Borrowed: {self.is_borrowed}")
 
-book1 = Book("Atomic Habit","James Clear",False)
+class Library:
+  def __init__(self):
+    self.books = []
 
-book1.display()
+  def borrow_book(self,book_id):
+    for book in self.books:
+      if book.book_id == book_id:
+
+library = Library() # it's create on library object
+
+book1= Book(1,"Deep work",299)
+book2= Book(1,"Let's try",299)
+
+library.books.append(book1)
+library.books.append(book2)
+
+for book in library.books:
+    book.display_book()
