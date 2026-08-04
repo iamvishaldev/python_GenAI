43ba9f27ca585d30e61590318684a28b58cda5f0
iamvishaldev <vishalyadavdev1@example.com>
2026-08-04 09:00:17 +0530

    test

--- Revision/exception.py
+++ Revision/exception.py
@@ -1,17 +1,52 @@
-with open("note.txt","w") as file:
-  file.write("I am learning python")
+# with open("note.txt","w") as file:
+#     file.write("I am learning the python")
 
-with open("note.txt","r") as file:
-  data = file.read()
-  print("Data after write")
-  print(data)
+# with open("note.txt","r") as file:
+#     data = file.read()
 
-with open("note.txt","a") as file:
-  file.write("\nLearn AI")
-  file.write("\nToday I learned File Handling")
+# print("Data after write")
+# print(data)
 
-with open("note.txt", "r") as file:
-    data = file.read()
+# with open("note.txt","a") as file:
+#     file.write("\nLearn AI")
+#     file.write("\nToday I learned File Handling.")
 
-print("\nData after Append:")
-print(data)
+# with open("note.txt","r") as file:
+#     data = file.readlines()
+
+# print("Data after append")
+
+# print(data)
+
+# class OutOfIngredientsError(Exception):
+#     pass
+
+# def make_chai(milk,sugar):
+#     if milk == 0 or sugar == 0:
+#         raise OutOfIngredientsError("Missing milk or sugar")
+#     print("chai is ready")
+
+# make_chai(0,1)
+
+class InsufficientBalanceError(Exception):
+    pass
+
+balance = 5000
+
+try:
+    amount = int(input("Enter the withdrawal amount :"))
+    
+    if amount > balance:
+        raise InsufficientBalanceError("Amount should not be greater then the balance")
+    balance-=amount
+    print(f"Your balance is {balance}")
+    
+except InsufficientBalanceError as e:
+    print(e)
+
+except ValueError:
+    print("Please enter a valid number.")
+
+finally:
+    print("Transaction Successful")
+    print("Thank you for using our ATM.")
