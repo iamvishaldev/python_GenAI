84856fd1541b6a8ddf8f1f39a0c5b3a359ad6df5
Vishal <vishalplexitech@gmail.com>
2026-08-13 17:25:06 +0530

    test

--- Revision/Decorators/dec.py
+++ Revision/Decorators/dec.py
@@ -1,7 +1,32 @@
+# A decorator in python is a function 
+# that takes another function add extra functionality to it 
+# and return the modified function without changinng the original function's code
 
-def greet():
-  print("Hello")
-def celebration(greet):
-  print("Hello Celebration",greet)
+# def my_decorator(func):
 
-greet()
+#   def wrapper():
+#     print("Before function run")
+#     func()
+#     print("After function run")
+
+#   return wrapper
+
+# @my_decorator
+# def greet():
+#   print("hello")
+
+# greet()
+
+# print(greet.__name__)
+
+from functools import wraps
+
+def my_decorator(func):
+
+  @wraps(func)
+  def wrapper():
+        print("Before")
+        func()
+        print("After")
+
+  return wrapper
